573d932ab316ab61fa376f871b25738cea35c18e
Mandakhgerel Ganbold <mandahganbold@gmail.com>
2020-12-29 22:09:28 -0800

    updated service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,14 +1,15 @@
-import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map'
 
 @Injectable()
 export class ProductService {
-  _albumUrl = '../assets/album.json';
+  private _albumUrl = '../assets/album.json';
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
+  getAlbum(id: number) : Observable<any>{
     return this._http.get(this._albumUrl).map(response => response.json());
   }
 }
