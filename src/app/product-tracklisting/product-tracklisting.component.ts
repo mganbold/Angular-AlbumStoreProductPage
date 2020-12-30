07e604a41145bd9c66345756364200e7491ae7dc
Mandakhgerel Ganbold <mandahganbold@gmail.com>
2020-12-30 11:39:48 -0800

    fix import

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,4 +1,4 @@
-import { ProductService } from './../product.service';
+import { ProductService } from '../product.service';
 import { Album } from '../album';
 import { Component, OnInit } from '@angular/core';
 
